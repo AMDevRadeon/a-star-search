7f2d10be7808bcfe29404551f4324bd4cf8f4690
Adrian Kulawik <a.kulawik8@gmail.com>
2023-05-21 14:26:21 +0200

    Add simple window

--- src/gui_application.cpp
+++ src/gui_application.cpp
@@ -1,5 +1,38 @@
 #include "gui_application.hpp"
 
+void Application::update()
+{
+	SDL_Event sdlEvent{};
+	SDL_WaitEvent(&sdlEvent);
+
+	if (sdlEvent.type == SDL_QUIT)
+		m_isRunning = false;
+}
+
+void Application::create_window(int width, int height)
+{
+	destroy_window();
+	constexpr auto pos = SDL_WINDOWPOS_UNDEFINED;
+
+	m_window = SDL_CreateWindow("A* Pathfinding", pos, pos, width, height, 0);
+	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void Application::destroy_window()
+{
+	if (m_renderer != nullptr)
+	{
+		SDL_DestroyRenderer(m_renderer);
+		m_renderer = nullptr;
+	}
+
+	if (m_window != nullptr)
+	{
+		SDL_DestroyWindow(m_window);
+		m_window = nullptr;
+	}
+}
+
 Application::Application()
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -9,6 +42,8 @@ Application::Application()
 
 Application::~Application()
 {
+	destroy_window();
+
 	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
@@ -16,5 +51,6 @@ Application::~Application()
 
 void Application::run()
 {
-
+	create_window(400, 400);
+	while (m_isRunning) update();
 }
--- src/gui_application.hpp
+++ src/gui_application.hpp
@@ -4,6 +4,15 @@
 
 class Application
 {
+	bool m_isRunning = true;
+
+	SDL_Window* m_window = nullptr;
+	SDL_Renderer* m_renderer = nullptr;
+
+	void update();
+	void create_window(int width, int height);
+	void destroy_window();
+
 	public:
 	Application();
 	~Application();
