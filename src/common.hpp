feeb8706b6dbe2db29fd117bceaff364c615f384
Adrian Kulawik <a.kulawik8@gmail.com>
2023-05-21 12:32:53 +0200

    Add common.hpp

---
+++ src/common.hpp
@@ -0,0 +1,37 @@
+enum State
+{
+	AVAILABLE, // dystans nieskonczony
+	OPEN,      // dystans okreslony
+	CLOSED,    // wezel wylaczony z algorytmu
+	PATH       // droga powrotna
+};
+
+struct Vertex
+{
+	int xpos = 0;
+	int ypos = 0;
+
+	float fcost = 0; // gcost + hcost
+	float gcost = 0; // dystans od poczatku
+	float hcost = 0; // heurystyka
+
+	bool is_active = true;
+	Vertex* previous = nullptr;
+	State state = AVAILABLE;
+
+	void Reset();
+};
+
+inline void Vertex::Reset()
+{
+	fcost = 0;
+	gcost = 0;
+	hcost = 0;
+
+	is_active = true;
+	previous = nullptr;
+	state = AVAILABLE;
+}
+
+void solve_astar(Vertex* vertices, Vertex* start, Vertex* stop, int xsize, int ysize, bool diagonal_allowed);
+
