6599a5f612919d09cc3186ce21609f922aea7e9f
Adrian Kulawik <a.kulawik8@gmail.com>
2023-06-06 17:08:46 +0200

    Add resize capability

--- src/gui_callback.cpp
+++ src/gui_callback.cpp
@@ -53,7 +53,41 @@ void callback_deactivate(Application& app) { app.m_mode = B_DEACTIVATE; }
 
 void callback_resize(Application& app)
 {
-	// TODO: Write implementation
+	int width, height;
+	SDL_HideWindow(app.m_window);
+
+	ask_for_width:
+	std::cout << "New width: ";
+	std::cin >> width;
+
+	if (width < 2)
+	{
+		std::cout << "Width must be at least 2\n";
+		goto ask_for_width;
+	}
+
+	ask_for_height:
+	std::cout << "New height: ";
+	std::cin >> height;
+
+	if (height < 2)
+	{
+		std::cout << "Height must be at least 2\n";
+		goto ask_for_height;
+	}
+
+	app.m_matrixWidth = width;
+	app.m_matrixHeight = height;
+
+	app.m_templatePath.clear();
+	app.reload();
+
+	SDL_Event sdlEvent{};
+	sdlEvent.type = SDL_MOUSEBUTTONUP;
+
+	SDL_ShowWindow(app.m_window);
+	SDL_PushEvent(&sdlEvent);
+	app.update();
 }
 
 void callback_randomize(Application& app)
